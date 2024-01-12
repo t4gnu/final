0b905169bdb750dc329ceb381c19d95eb20c325d
t4gnu <133129091+t4gnu@users.noreply.github.com>
2024-01-12 12:26:36 +0900

    Update db-connect.php

--- src/db-connect.php
+++ src/db-connect.php
@@ -1,6 +1,6 @@
 <?php
-    const SERVER = 'mysql214.phy.lolipop.lan';
-    const DBNAME = 'LAA1516828-shop';
+    const SERVER = 'mysql220.phy.lolipop.lan';
+    const DBNAME = 'LAA1516828-final';
     const USER = 'LAA1516828';
     const PASS = 'Pass0318';
 
